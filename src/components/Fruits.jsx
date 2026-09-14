6e678d1ba27d80fe404ccee058fdac0def7bcb2b
Phil-osophyx <akshagarwalpro@gmail.com>
2026-09-14 21:07:23 +0530

    Rendering Array of Objects

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,11 +1,20 @@
 export default function Fruits() {
-  const fruits = ["Apple", "Mango", "Orange", "Banana", "Guava"];
+  // const fruits = ["Apple", "Mango", "Orange", "Banana", "Guava"];
+
+  const fruits = [
+    { name: "Apple", price: 10, emj: "🍎" },
+    { name: "Mango", price: 7, emj: "🥭" },
+    { name: "Banana", price: 3, emj: "🍌" },
+  ];
   return (
     <div>
-        <ul>
-      {fruits.map((frt) => (
-        <li key='frt' >{frt}</li>
-      ))}
+      <ul>
+        {fruits.map((frt) => (
+          <li key={frt.name}>
+            {" "}
+            {frt.emj} {frt.name} {frt.price}
+          </li>
+        ))}
       </ul>
     </div>
   );
