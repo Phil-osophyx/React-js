26dd0a1749acd4617d1d37816001878059563bba
Phil-osophyx <akshagarwalpro@gmail.com>
2026-09-17 21:45:08 +0530

    Conditionally rendering msg using ternary

--- src/components/Fruit.jsx
+++ src/components/Fruit.jsx
@@ -1,14 +1,9 @@
-export default function Fruit({ name, price, emj }) {
+export default function Fruit({ name, price, emj, soldout}) {
   return (
-      < >
-      {/* {emj} {name} {price}{" "} */}
-      {price > 5 ? (
-          <li>
-          {emj} {name} {price}
-        </li>
-      ) : (
-        ""
-      )}
-  </>
+    <>
+      <li>
+        {emj} {name} {price} {soldout ? "soldOut": ""}
+      </li>
+    </>
   );
 }
--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,18 +1,18 @@
-import Fruit from "./Fruit"
+import Fruit from "./Fruit";
 
 export default function Fruits() {
   // const fruits = ["Apple", "Mango", "Orange", "Banana", "Guava"];
 
   const fruits = [
-    { name: "Apple", price: 10, emj: "🍎" },
-    { name: "Mango", price: 7, emj: "🥭" },
-    { name: "Banana", price: 3, emj: "🍌" },
+    { name: "Apple", price: 10, emj: "🍎", soldout: false },
+    { name: "Mango", price: 7, emj: "🥭", soldout: true },
+    { name: "Banana", price: 3, emj: "🍌", soldout: false },
   ];
   return (
     <div>
       <ul>
         {fruits.map((fruit) => (
-          <Fruit name={fruit.name} price={fruit.price} emj={fruit.emj} />
+          <Fruit name={fruit.name} price={fruit.price} emj={fruit.emj} soldout={fruit.soldout} />
         ))}
       </ul>
     </div>
